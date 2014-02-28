457ff0c77c74f177c5093a65001081de9e09b5c7
Josh Flayhart <jflayhart@zappend.com>
2014-02-28 02:22:05 -0600

    Changes made by Josh Flayhart (jflayhart@zappend.com).

--- Hackathon/scripts/listing.js
+++ Hackathon/scripts/listing.js
@@ -17,16 +17,16 @@ function getListing(index) {
             var listing = data['listing'];
             console.log('Length: ' + listing.length);
             for(i=0;i<listing.length;i++) {
-                // array objects
+                // array objects, replace with empty value if undefined
                 var Location = listing[i]['Location']['address'];
-                var LocationKey = listing[i]['ListingKey'];
-                var media = listing[i]['Media'][0]['file'];
                 // array values for the objects
-                var streetNumber = Location['StreetNumber'];
-                var streetName = Location['StreetName'];
-                var city = Location['City'];
-                var state = Location['StateOrProvince'];
-                var postal = Location['PostalCode'];
+                var LocationKey = listing[i]['ListingKey'] || '';
+                var media = listing[i]['Media'][0]['file'] || '';
+                var streetNumber = Location['StreetNumber'] || '';
+                var streetName = Location['StreetName'] || '';
+                var city = Location['City'] || '';
+                var state = Location['StateOrProvince'] || '';
+                var postal = Location['PostalCode'] || '';
                 var fullAddress = streetNumber+' '+streetName+'\n'+city+', '+state+'\n'+postal;
                 //console.log(listing[i]);
                 //console.log(Location);
@@ -53,17 +53,44 @@ function getListing(index) {
 }
 
 function moreInfo(locationKey) {
+    $('#local-view,#local-image,#agent-view').empty();
     $.ajax({
     type: 'GET',
     url: 'https://api.solidearth.com/v1/listing/baarmls/'+locationKey+'?format=json&api_key=6gphe6d6pck3km5anrjrcqcf',
     timeout: 20000, //20 seconds, for testing purposes
     dataType: 'jsonp',
     success: function(data) {
-        var listingPrice = data['listingPricing']['listPrice'];
-        var listingImg = data['Media'][0]['file'];
+        // array objects, replace with empty value if undefined
+        var Location = data['Location']['address'];
+        var AgentOffice = data['AgentOffice'];
+        // array values for the objects
+        var streetNumber = Location['StreetNumber'] || '';
+        var streetName = Location['StreetName'] || '';
+        var city = Location['City'] || '';
+        var state = Location['StateOrProvince'] || '';
+        var postal = Location['PostalCode'] || '';
+        var fullAddress = streetNumber+' '+streetName+'\n'+city+', '+state+'\n'+postal;
+        var listingPrice = data['listingPricing']['listPrice'] || '';
+        var listingImg = data['Media'][0]['file'] || '';
+        var listingDesc = data['Media'][0]['desc'] || '';
+        var structureBed = data['Structure']['BedroomsTotal'] || '';
+        var structureBath = data['Structure']['BathroomsTotal'] || '';
+        var agentImg = AgentOffice['ListAgent']['Image'] || '';
+        var agentName = AgentOffice['ListAgent']['FullName'] || '';
+        var officeName = AgentOffice['ListOffice']['Name'] || '';
+        var officePhone = AgentOffice['ListOffice']['Phone'] || '';
+        var officeEmail = AgentOffice['ListOffice']['Email'] || '';
     
         $('#local-image').html('<img src="http:'+listingImg+'" />');
-        $('#local-view').html('<p>Price: $'+listingPrice+'/mo</p>');
+        $('#local-view').append('<p>'+listingDesc+'</p>');
+        $('#local-view').append('<h4>Address</h4> <p>'+fullAddress+'</p>');
+        $('#local-view').append('<h4>Price</h4> <p>$'+listingPrice+'/mo</p>');
+        $('#local-view').append('<h4>Bed/Bath</h4> <p>'+structureBed+'/'+structureBath+'</p>');
+        $('#agent-view').append('<img align="left" src="http:'+agentImg+'" />');
+        $('#agent-view').append('<p><strong>'+agentName+'</strong></p>');
+        $('#agent-view').append('<p>'+officeName+'</p>');
+        $('#agent-view').append('<p>'+officePhone+'</p>');
+        $('#agent-view').append('<p>'+officeEmail+'</p>');
     },
     error: function (xhr, status, error) {
         if(status == "timeout") {
