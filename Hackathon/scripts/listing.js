8a0739291d454478ce7811a9ecf9016ec9a3eaec
Josh Flayhart <jflayhart@zappend.com>
2014-02-28 00:02:29 -0600

    Changes made by Josh Flayhart (jflayhart@zappend.com).

--- Hackathon/scripts/listing.js
+++ Hackathon/scripts/listing.js
@@ -11,9 +11,8 @@ $.ajax({
         for(i=0;i<listing.length;i++) {
         	// array objects
         	var Location = listing[i]['Location']['address'];
-        	var AgentOffice = listing[i]['AgentOffice']['ListAgent'];
+            var LocationKey = listing[i]['ListingKey'];
         	var media = listing[i]['Media'][0]['file'];
-        	
         	// array values for the objects
         	var streetNumber = Location['StreetNumber'];
         	var streetName = Location['StreetName'];
@@ -25,8 +24,15 @@ $.ajax({
         	//console.log(Location);
         	//console.log(fullAddress);
         	//console.log("http:"+media);
-        	$('#listing').append('<li>' + '<img align="left" src="http:'+media+'" />'+fullAddress+'</li>');
+        	$('#listing').append('<li>' + '<img align="left" src="http:'+media+'" />'+fullAddress+
+            '<br /><br /><a class="more-info '+LocationKey+'" href="#secondview">More Info</a></li>');
         }
+        $('a.more-info').click(function() {
+        	var myClasses = this.classList;
+            locationKey = myClasses[1];
+          	moreInfo(locationKey);
+          	return true;
+        });
     },
     error: function (xhr, status, error) {
         if(status == "timeout") {
@@ -36,3 +42,26 @@ $.ajax({
         }
     }
 });
+
+function moreInfo(locationKey) {
+    $.ajax({
+    type: 'GET',
+    url: 'https://api.solidearth.com/v1/listing/baarmls/'+locationKey+'?format=json&api_key=6gphe6d6pck3km5anrjrcqcf',
+    timeout: 20000, //20 seconds, for testing purposes
+    dataType: 'jsonp',
+    success: function(data) {
+        var listingPrice = data['listingPricing']['listPrice'];
+        var listingImg = data['Media'][0]['file'];
+    
+        $('#local-image').html('<img src="http:'+listingImg+'" />');
+        $('#local-view').html('<p>'+listingPrice+'</p>');
+    },
+    error: function (xhr, status, error) {
+        if(status == "timeout") {
+            alert("Connection has timed out.");
+        } else {
+            alert(xhr.status + " " + status + " " + error);
+        }
+    }
+});
+}
